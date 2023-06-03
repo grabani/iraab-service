25956d3a027cb73ea57c3a0469f3c3dea46f2588
Hady Walied <hadywalied@pop-os.localdomain>
2023-06-04 01:54:46 +0300

    now gathered the files in one out.json file

--- scripts/convert_all_to_one_file.py
+++ scripts/convert_all_to_one_file.py
@@ -4,19 +4,13 @@ import os
 from scripts.scrap import extract_text_from_path, process_text
 
 if __name__ == '__main__':
-    for root, dirs, files in os.walk("../data"):
-        path = root.split(os.sep)
-        out_path = os.path.join(os.path.dirname(__file__), 'out')
-        os.makedirs(out_path, exist_ok= True)
-        print("converting files")
-        print((len(path) - 1) * '---', os.path.basename(root))
+    collected = {}
+    for root, dirs, files in os.walk("out"):
         for file in files:
-            print(len(path) * '---', file)
-            filepath = os.path.join(root, file)
-            text = extract_text_from_path(filepath)
+            with open(os.path.join(root, file), 'r', encoding='utf-8') as f:
+                text = ''.join(f.readlines())
+                loaded = json.loads(text)
+                collected.update(loaded)
 
-            parsed_text = process_text(text)
-
-            json_out_file = os.path.join(out_path, f'{os.path.basename(file)}.json')
-            with open(json_out_file, 'w', encoding='utf-8') as f:
-                json.dump(parsed_text, f, ensure_ascii=False)
+    with open('out.json', 'w', encoding='utf-8') as f:
+        json.dump(collected, f, ensure_ascii=False)
